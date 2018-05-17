9ad6acf501b368e0c8da5e8edd856a6aa905a764
meetwatermelon <meetwatermelon@163.com>
2018-05-17 13:10:47 +0800

    0517备份

--- src/main/java/cn/imust/blog/controller/ArticleController.java
+++ src/main/java/cn/imust/blog/controller/ArticleController.java
@@ -1,7 +1,49 @@
 package cn.imust.blog.controller;
 
+import cn.imust.blog.entity.Article;
+import cn.imust.blog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping (value = "/article")
 public class ArticleController {
+
+    @Autowired private ArticleService articleService;
+
+    @RequestMapping (value = "addarticle")
+    public String addArticle(){
+       return "admin/article/addarticle";
+    }
+
+    @RequestMapping (value = "savearticle")
+    public String saveArticle(Article article){
+        articleService.saveArticle(article);
+        return "redirect:articlelist";
+    }
+
+    @RequestMapping (value = "findarticle")
+    public String findArticle(Model model){
+        List<Article> list = articleService.findArticle();
+        model.addAttribute("list",list);
+        return "admin/article/articlelist";
+    }
+
+    @RequestMapping (value = "deletearticle/{id}")
+    public String deleteArticle(@PathVariable int id){
+        articleService.deleterArticle(id);
+        return "redirect:articlelist";
+    }
+
+    @RequestMapping (value = "updatearticle")
+    public String updateArticle(){
+        return "admin/article/updatearticle";
+    }
+
+
 }
--- src/main/java/cn/imust/blog/service/ArticleService.java
+++ src/main/java/cn/imust/blog/service/ArticleService.java
@@ -1,10 +1,38 @@
 package cn.imust.blog.service;
 
+import cn.imust.blog.entity.Article;
+import cn.imust.blog.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class ArticleService {
+    @Autowired private ArticleRepository articleRepository;
+
+    public void saveArticle(Article article){
+        articleRepository.save(article);
+    }
+
+    public void deleterArticle(int id){
+        articleRepository.delete(id);
+    }
+
+    public List<Article> findArticle(){
+        return articleRepository.findAll();
+    }
+
+    public Article getOneArticle(int id){
+        return articleRepository.getOne(id);
+    }
+
+    public void updateArticle(Article article){
+        articleRepository.saveAndFlush(article);
+    }
+
+
+
 }
